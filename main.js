2594d21dc6030814bff3ee0d30188f6fa2239bda
itsmahammad <mahammadjm-azmpa201@code.edu.az>
2025-11-30 21:27:34 +0400

    added delete and elements with classes

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 function addTask() {
     let task = document.getElementById('input')
     let taskValue = task.value
@@ -7,13 +8,26 @@ function addTask() {
     const label = document.createElement('label')
     label.className = "task"
     const input = document.createElement('input')
+    input.type = 'checkbox'
     input.className = "task-checkbox"
     const span = document.createElement('span')
+    span.className = 'task-text'
+    const button = document.createElement('button')
+    button.className = 'delete'
+    button.textContent = 'x'
+    span.textContent = taskValue
 
-    li.textContent = taskValue
+    label.appendChild(input)
+    label.appendChild(span)
+    li.appendChild(label)
+    li.appendChild(button)
+
     if (taskValue != '' && taskValue.length >= 3 && taskValue != '   ') {
         ul.appendChild(li)
-        ul.appendChild()
-    }
 
+    }
+    button.addEventListener("click", () => {
+        li.remove()
+    })
 }
+
